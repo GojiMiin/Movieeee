cc217727c915842113cda733c7a60ee9cf6ddef5
GojiMiin <got.j@windowslive.com>
2021-01-27 16:25:50 +0700

    use AppBar as navbar

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,19 +1,50 @@
 import React from "react";
-import { Container, Box } from '@material-ui/core';
+import {AppBar, Toolbar, List, ListItem, ListItemText, makeStyles, Container, Typography, Box} from '@material-ui/core';
 import '../scss/Header.scss'
 
-class Header extends React.Component {
-    render() {
-        return(
-            <Container className="TopBar">
-                <Box>
-                    <p>Movie Classification</p>
-                </Box>
-                <Box>
-                    <p>Hello</p>
-                </Box>
-            </Container>
-        );
+const useStyles = makeStyles({
+    navDisplayFlex: {
+        display: `flex`,
+        justifyContent: `space-between`
+    },
+    linkText: {
+        textDecoration: `none`,
+        textTransform: `uppercase`,
+        color: `white`
     }
+});
+
+const navlinks = [
+    { title: "Year", path: "/year"},
+    {title: "Genre", path: "/genre"},
+    {title: "Rate", path: "/rate"}
+]
+
+const Header = () => {
+    const classes = useStyles();
+    return(
+        <AppBar>
+            <Toolbar>
+                <Container fixed className={classes.navDisplayFlex}>
+                    <Container fixed className="titleBox">
+                        <Typography variant="h6">
+                            Movie Classification
+                        </Typography>
+                    </Container>
+                    <List component="nav"
+                          aria-labelledby="main navigation"
+                          className={classes.navDisplayFlex}>
+                        {navlinks.map(({ title, path }) => (
+                            <a href={path} key={title} className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary={title} />
+                                </ListItem>
+                            </a>
+                        ))}
+                    </List>
+                </Container>
+            </Toolbar>
+        </AppBar>
+    );
 }
 export default Header;
